883873173810eb6eea3a01cfe300bfacb239eae6
Vanessa Zyto <vanessazyto@Vanessas-MacBook-Air.local>
2025-10-29 21:47:33 +0100

    Assignement finished

--- tasks.py
+++ tasks.py
@@ -1,20 +1,29 @@
-import numpy
+import numpy as np
 
 # Follow the tasks below to practice basic Python concepts.
 # Write your code in between the dashed lines.
 # Don't import additional packages. Numpy suffices.
 
-# Task 1: 
+# Task 1:
 # Instructions:
-#Write a function that takes one numeric argument as input. 
+#Write a function that takes one numeric argument as input.
 #If the number is larger than zero, the function should return 1, otherwise is should return -1.
 #The name of the function should be step
 
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(number):
+    if number > 0:
+        return 1
+    else:
+        return -1
 
+# testing
+# test_1 = step(5)
+# print(test_1)
+# test_2 = step(-5)
+# print(test_2)
 
 # -----------------------------------------------
 
@@ -28,7 +37,15 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
+def ReLu(arr, cutoff = 0):
+    new_array = arr.copy()
+    mask = arr < cutoff
+    new_array[mask] = cutoff
+    return new_array
+
+# testing
+#test_3 = ReLu(arr = np.array([1,2,3,4,5,6]), cutoff = 3)
+#print(test_3)
 
 
 # -----------------------------------------------
@@ -44,7 +61,24 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(X, y):
+    multiplication = np.matmul(X, y)
 
+    return ReLu(multiplication)
+
+
+# testing
+# X = np.array([
+#     [1, -2, 3],
+#     [0, 4, -1],
+#     [2, 2, 2]
+# ])
+# y = np.array([1, 2, -1])
+#
+# #test_4 = neural_net_layer(X, y)
+# print(test_4)
 
 # ------------------------------------------
+
+# Disclaimer: all of the functions assume that the input data types and shapes
+# are as specified in the task instructions (e.g., valid numeric or NumPy arrays).
